c5f937baeb77f306213ef0cf14f283747713f204
ronaldsavino <ronaldsavino@gmail.com>
2016-02-06 11:38:36 +0100

    seconda

--- Ciao/src/Ciao.java
+++ Ciao/src/Ciao.java
@@ -50,7 +50,7 @@ public class Ciao {
 		btnBottone.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				MessageDialog.openInformation(shell, "ehi", "Ciao Mondo, crude");
+				MessageDialog.openInformation(shell, "ehi", "Ciao Mondo, crudele");
 			}
 		});
 		btnBottone.setBounds(166, 108, 75, 25);
